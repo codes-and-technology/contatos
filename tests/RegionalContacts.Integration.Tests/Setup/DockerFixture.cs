f1f9c1418d18f0a8a74dc8fb24cb56ba42aec996
Eduardo Rodrigues Neto <eduardo.neto@rsinet.com.br>
2024-07-21 17:21:33 -0300

    Ajustes das portas

--- tests/RegionalContacts.Integration.Tests/Setup/DockerFixture.cs
+++ tests/RegionalContacts.Integration.Tests/Setup/DockerFixture.cs
@@ -47,7 +47,7 @@ namespace RegionalContacts.Integration.Tests.Setup
                     {
                         PortBindings = new Dictionary<string, IList<PortBinding>>
                     {
-                        { "1433/tcp", new List<PortBinding> { new PortBinding { HostPort = "66666" } } }
+                        { "1433/tcp", new List<PortBinding> { new PortBinding { HostPort = "1533" } } }
                     }
                     }
                 });
@@ -76,7 +76,7 @@ namespace RegionalContacts.Integration.Tests.Setup
                     {
                         PortBindings = new Dictionary<string, IList<PortBinding>>
                     {
-                        { "6379/tcp", new List<PortBinding> { new PortBinding { HostPort = "69999" } } }
+                        { "6379/tcp", new List<PortBinding> { new PortBinding { HostPort = "6380" } } }
                     }
                     }
                 });
